258403eee6f8607d853700e6efb0da786fdcf18c
shirishpothi <shirishpothi@users.noreply.github.com>
2026-06-18 21:49:01 +0800

    [skip ci] Differentiate organize rename icon

--- Sources/SortyLib/Models/AIConfig.swift
+++ Sources/SortyLib/Models/AIConfig.swift
@@ -418,7 +418,7 @@ public enum OrganizationMode: String, Codable, CaseIterable, Sendable {
     public var iconName: String {
         switch self {
         case .organize: return "folder.badge.plus"
-        case .organizeAndRename: return "folder.badge.gearshape"
+        case .organizeAndRename: return "arrow.left.arrow.right"
         case .renameOnly: return "pencil.line"
         }
     }
