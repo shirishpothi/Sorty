29509465032fb0961b8e9335b35eca5d63563313
shirishpothi <shirishpothi@users.noreply.github.com>
2026-09-03 12:43:25 +0800

    refactor: colocate onboarding validation [skip ci]

--- Sources/SortyLib/Views/OnboardingView.swift
+++ Sources/SortyLib/Views/OnboardingView.swift
@@ -525,6 +525,35 @@ private struct OnboardingCompletionDestination: View {
 
 // MARK: - Onboarding Step Enum
 
+public struct OnboardingStepValidationResult: Equatable, Sendable {
+    public let canAdvance: Bool
+    public let message: String?
+
+    public init(canAdvance: Bool, message: String? = nil) {
+        self.canAdvance = canAdvance
+        self.message = message
+    }
+
+    public static let valid = OnboardingStepValidationResult(canAdvance: true)
+
+    public static func blocked(_ message: String) -> OnboardingStepValidationResult {
+        OnboardingStepValidationResult(canAdvance: false, message: message)
+    }
+}
+
+public struct OnboardingStepValidationContext: Equatable, Sendable {
+    public let providerSetupStatus: ProviderSetupStatus
+    public let hasRequiredPermissions: Bool
+
+    public init(
+        providerSetupStatus: ProviderSetupStatus,
+        hasRequiredPermissions: Bool
+    ) {
+        self.providerSetupStatus = providerSetupStatus
+        self.hasRequiredPermissions = hasRequiredPermissions
+    }
+}
+
 enum OnboardingStep: Int, CaseIterable {
     case provider = 0
     case permissions = 1
