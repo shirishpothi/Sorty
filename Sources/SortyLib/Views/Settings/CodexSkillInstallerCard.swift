004d1f60e411c8fbe1494dde9ebcc708287f75b2
shirishpothi <shirishpothi@users.noreply.github.com>
2026-09-05 13:34:19 +0800

    Use cube icon for Sorty for Codex card [skip ci]

--- Sources/SortyLib/Views/Settings/CodexSkillInstallerCard.swift
+++ Sources/SortyLib/Views/Settings/CodexSkillInstallerCard.swift
@@ -10,7 +10,7 @@ struct CodexSkillInstallerCard: View {
 
     var body: some View {
         HStack(alignment: .top, spacing: 12) {
-            Image(systemName: "sparkles.rectangle.stack")
+            Image(systemName: "cube")
                 .font(.system(size: 16, weight: .semibold))
                 .foregroundStyle(.indigo)
                 .frame(width: 28, height: 28)
